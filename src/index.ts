d2ebe07274de5bdcb838db3edd671d6f3d4361f3
yanquanfahei <1449826851@qq.com>
2023-02-15 19:12:41 +0800

    feat: prompts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
+import prompts from 'prompts'
+import chalk from 'chalk'
+import validatePkg from 'validate-npm-package-name'
+
+const log = console.log
+
+const templates = []
 
 async function init () {
-  console.log(234)
+  const result = await prompts([
+    {
+      type: 'text',
+      name: 'projectName',
+      message: 'Project name',
+      validate: (value) => {
+        const { validForNewPackages, validForOldPackages, errors, warnings } = validatePkg(value)
+        if (validForNewPackages || validForOldPackages) return true
+        return (errors || warnings || 'Invalid package.json name').toString()
+      }
+    },
+    {
+      type: 'select',
+      name: 'framework',
+      message: 'Select a framework',
+      choices: []
+    }
+  ])
+  log(chalk.blue(JSON.stringify(result)), 'result')
 }
 
 init().catch((err) => {
