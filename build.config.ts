9aeec11841cc74aaf06684bf12c385902d120dbb
yanquanfahei <1449826851@qq.com>
2023-02-24 08:52:15 +0800

    chore: update build config

--- build.config.ts
+++ build.config.ts
@@ -3,9 +3,5 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: ['src/index'],
   clean: true,
-  failOnWarn: false,
-  rollup: {
-    emitCJS: true,
-    dts: {}
-  }
+  failOnWarn: false
 })
