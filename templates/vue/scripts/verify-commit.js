ab8de4891b8a52b2bd5a4d919752ece02aa18131
yanquanfahei <1449826851@qq.com>
2023-03-12 12:10:14 +0800

    feat: vue template

---
+++ templates/vue/scripts/verify-commit.js
@@ -0,0 +1,27 @@
+// @ts-check
+import { readFileSync } from 'node:fs'
+import path from 'node:path'
+import chalk from 'chalk'
+
+const msgPath = path.resolve('.git/COMMIT_EDITMSG')
+const msg = readFileSync(msgPath, 'utf-8').trim()
+
+const commitRE = /^(feat|fix|docs|style|refactor|perf|test|build|ci|chore|release|merge)(\(.+\))?: .{1,50}/
+
+if (!commitRE.test(msg)) {
+  console.log()
+  console.error(
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
+      'invalid commit message format.'
+    )}\n\n` +
+      chalk.red(
+        '  Proper commit message format is required for automated changelog generation. Examples:\n\n'
+      ) +
+      `    ${chalk.green('feat(compiler): add \'comments\' option')}\n` +
+      `    ${chalk.green(
+        'fix(methods): handle events on blur (close #28)'
+      )}\n\n` +
+      chalk.red('  See .github/commit-convention.md for more details.\n')
+  )
+  process.exit(1)
+}
---
+++ templates/vue/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+</style>
